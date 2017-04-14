9ca9ee5c09c503a4d9c4a11aa107def8cdd6f751
Ryan DuVal <futzlarson@gmail.com>
2017-04-14 16:11:26 -0400

    ADMIN - Fixed count and display for zero pending - Reworked fields

--- admin.php
+++ admin.php
@@ -3,12 +3,14 @@
 require_once "/var/www/html/vendor/autoload.php";
 
 $collection = (new MongoDB\Client)->avlfilm->actors;
+$filter = [ 'approved' => false ];
 $pending = $collection->find(
-    [ 'approved' => false ],
+    $filter,
     [
         'sort' => [ 'name' => 1 ]
     ]
 );
+$results = $collection->count($filter);
 
 include '_header.php';
 
@@ -19,48 +21,56 @@ include '_header.php';
         margin-bottom: 5px;
     }
 
+/*
     footer {
         bottom: 0;
         left: 0;
         position: absolute;
         right: 0;
     }
+*/
 </style>
 
 <div class="content">
-    <h1>Pending Actors: <?= count($pending) ?></h1>
+    <h1>Pending Actors: <?= $results ?></h1>
 
-    <table border="1">
-        <tr>
-            <th>Name</th>
-            <th>Height</th>
-            <th>Hair color</th>
-            <th>Hair length</th>
-            <th>Eyes</th>
-            <th>Headshot</th>
-            <th>Resume</th>
-            <th>Contact</th>
-            <th>Comments</th>
-            <th>Actions</th>
-        </tr>
-        <?php foreach ($pending as $p) { ?>
+    <?php if ($results) { ?>
+        <table border="1">
             <tr>
-                <?php foreach (array('name', 'height', 'hair_color', 'hair_length', 'eyes', 'headshot', 'resume', 'contact', 'comments') as $f) { ?>
-                    <td>
-                    <?php if ($f == 'headshot' || $f == 'resume') { ?>
-                        <a href="/uploads/<?= $p->$f ?>"><?= $p->$f ?></a>
-                    <?php } else { ?>
-                        <?= $p->$f ?>
-                    <?php } ?>
-                    </td>
-                <?php } ?>
-                <td>
-                    <button>Approve</button>
-                    <button>Delete</button>
-                </td>
+                <th>Name</th>
+                <th>Height</th>
+                <th>Hair&nbsp;color</th>
+                <th>Hair&nbsp;length</th>
+                <th>Eyes</th>
+                <th>Headshot</th>
+                <th>Resume</th>
+                <!--<th>Contact</th>-->
+                <th>Comments</th>
+                <th>Actions</th>
             </tr>
-        <?php } ?>
-    </table>
+            <?php foreach ($pending as $p) { ?>
+                <tr>
+                    <?php foreach (array('name', 'height', 'hair_color', 'hair_length', 'eyes', 'headshot', 'resume', 'comments') as $f) { ?>
+                        <td>
+                        <?php if ($f == 'headshot') { ?>
+                            <a href="<?= $p->$f ?>"><?= $p->$f ?></a>
+                        <?php } else if ($f == 'resume') { ?>
+                            <a href="<?= $p->$f ?>">resume</a>
+                        <?php } else if (isset($p->$f)) { ?>
+                            <?= $p->$f ?>
+                        <?php } ?>
+                        </td>
+                    <?php } ?>
+                    <td>
+                        <button>Approve</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    <?php } else { ?>
+        <p>Aww.</p>
+    <?php } ?>
 </div>
 
 <?php include '_footer.php' ?>
