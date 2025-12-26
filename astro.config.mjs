fea7b911a9b2393da4c78e7c3ed22181a2095b04
Ryan DuVal <futzlarson@gmail.com>
2025-12-26 17:24:26 -0500

    Fix warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
