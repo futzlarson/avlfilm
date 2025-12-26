89ee9becdc83589a4680284c42dc88ab8eef08d5
Ryan DuVal <futzlarson@gmail.com>
2025-12-26 16:31:52 -0500

    And fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
